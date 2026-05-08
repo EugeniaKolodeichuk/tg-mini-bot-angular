ecada6934dc8a902c7da13a2e968d348e387ce45
eugenia.liushuk <kolodeichuk.e@gmail.com>
2026-05-09 00:22:59 +0300

    + routes with page components

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -6,11 +6,21 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CommonModule],
-  template: `
-  <h1>{{title | uppercase}}</h1>
-  <button (click)="title = 'Updated Title'">Update Title</button>
-  `,
+  template: `<router-outlet/>`,
+  //for educational purposes only, not part of the actual app
+  // template: `
+  // <h1>{{title | uppercase}}</h1>
+  // <button (click)="title = 'Updated Title'">Update Title</button>
+  // <ul>
+  // @for(item of items; track item) {
+  //   <li>{{item}}</li>
+  // }
+  //   @if(title === 'Updated Title') {
+  //   <hr/>}
+  // </ul>
+  // `,
 })
 export class AppComponent {
   title = 'webapp';
+  items = [1, 2, 3, 4, 5]
 }
